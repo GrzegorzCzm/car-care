6399237419d7e0d36f607476f5dc8fc8c808c50d
Grzegorz Czmielewski <grzegorz.czmielewski@etteplan.com>
2020-02-23 22:27:59 +0100

    refactor - part III

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -3,7 +3,7 @@ import { TOGGLE_MENU } from '../actions/navigation';
 export default function (state = { isMenuShown: false }, action) {
   switch (action.type) {
     case TOGGLE_MENU:
-      return { ...state, isMenuShown: !state.isMenuShown };
+      return { isMenuShown: !state.isMenuShown };
     default:
       return state;
   }
