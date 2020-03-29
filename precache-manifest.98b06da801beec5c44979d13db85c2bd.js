c60fa01861ef605f6091e3c592fdce985b1001df
Grzegorz Czmielewski <grzegorz.czmielewski@etteplan.com>
2020-03-29 22:05:45 +0200

    Updates

---
+++ precache-manifest.98b06da801beec5c44979d13db85c2bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af25e899ebe05536f8bf7123e3e92574",
+    "url": "/car-care/index.html"
+  },
+  {
+    "revision": "57687de0a0558c549545",
+    "url": "/car-care/static/css/3.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5a08188417a1bdd76048",
+    "url": "/car-care/static/css/main.fce953d1.chunk.css"
+  },
+  {
+    "revision": "eb304463531c40adf867",
+    "url": "/car-care/static/js/2.c4114fa6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/car-care/static/js/2.c4114fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57687de0a0558c549545",
+    "url": "/car-care/static/js/3.7da0caea.chunk.js"
+  },
+  {
+    "revision": "0a9374b4bbfbbf94ac5ef4bff0606566",
+    "url": "/car-care/static/js/3.7da0caea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86be4b4eb75ce7a764a5",
+    "url": "/car-care/static/js/4.649fbabb.chunk.js"
+  },
+  {
+    "revision": "5a08188417a1bdd76048",
+    "url": "/car-care/static/js/main.0b2f60c0.chunk.js"
+  },
+  {
+    "revision": "099ebb8cb112fe8fd345",
+    "url": "/car-care/static/js/runtime-main.6be12e17.js"
+  }
+]);
